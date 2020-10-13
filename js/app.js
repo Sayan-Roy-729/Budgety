2fb453cc4dbb5ea986efe54051b9c83676fd5a3c
mohini mahato <mohinimahatowork@gmail.com>
2020-10-13 21:04:40 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+alert("email authentication is having some trouble in this moment, click on the signup button and get signin via Google . Sorry for the inconvinence we will resolve the bug soon. Thank You")
 const signinbtn = document.querySelector('#sign_in_btn');
 const signupbtn = document.querySelector('#sign_up_btn');
 const container = document.querySelector('.container');
